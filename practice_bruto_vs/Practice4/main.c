eedeb28cc1c9e8f3f2893df14f4085fad3a459a8
Dé-Jà-Vu <26201406+iamdeja@users.noreply.github.com>
2020-01-09 20:24:13 +0200

    Bruto isn't burrito

--- practice_bruto_vs/Practice4/main.c
+++ practice_bruto_vs/Practice4/main.c
@@ -4,18 +4,19 @@
 
 typedef struct {
 	char* pFirstName, * pMiddleName, * pLastName;
-	int Burrito, TaxCoefficient, Netto;
+	int Brutto, TaxCoefficient, Netto;
 } EMPLOYEE;
 
 EMPLOYEE* Exam(char*);
 int MiddleName(char*);
+void StructUpdate(char**, char**, int*, char*);
 
 int main()
 {
 	char input[] = "John James Smith 15000";
 	EMPLOYEE* pOutput = Exam(input);
 	// %% is escape sequence for %, no space between %s%s as this is handled by pOutput
-	printf("%s %s%s %d %d%% %d", pOutput->pFirstName, pOutput->pMiddleName, pOutput->pLastName, pOutput->Burrito, pOutput->TaxCoefficient, pOutput->Netto);
+	printf("%s %s%s %d %d%% %d", pOutput->pFirstName, pOutput->pMiddleName, pOutput->pLastName, pOutput->Brutto, pOutput->TaxCoefficient, pOutput->Netto);
 
 	// freeing stuff
 	free(pOutput->pFirstName); pOutput->pFirstName = NULL;
@@ -41,26 +42,23 @@ EMPLOYEE* Exam(char* pInput) {
 
 	// middle name | Recommendation: make a function to handle
 	if (namebool) {
-		pIndex = strpbrk(pIndex + 1, apb);
-		pOutdex = strchr(pIndex, ' ');
-		len = pOutdex - pIndex + 1; // counting the space, for modular display
+		StructUpdate(&pIndex, &pOutdex, &len, apb);
+		len++; // counting the space, for modular display
 		pOutput->pMiddleName = (char*)calloc(len + 1, sizeof(char));
 		strncpy_s(pOutput->pMiddleName, len + 1, pIndex, len);
 	}
 	else
 		pOutput->pMiddleName = ""; // no middle name
 
-	// last name | Recommendation: make a function to handle
-	pIndex = strpbrk(pIndex + 1, apb);
-	pOutdex = strchr(pIndex, ' ');
-	len = pOutdex - pIndex;
+	// last name
+	StructUpdate(&pIndex, &pOutdex, &len, apb);
 	pOutput->pLastName = (char*)calloc(len + 1, sizeof(char));
 	strncpy_s(pOutput->pLastName, len + 1, pIndex, len);
 	pOutdex = NULL;
 
 	// price
 	pIndex = strpbrk(pInput, "0123456789");
-	pOutput->Burrito = atoi(pIndex);
+	pOutput->Brutto = atoi(pIndex);
 	if (atoi(pIndex) > 10000)
 		pOutput->TaxCoefficient = 25;
 	else if (atoi(pIndex) > 7500)
@@ -69,7 +67,7 @@ EMPLOYEE* Exam(char* pInput) {
 		pOutput->TaxCoefficient = 15;
 	else
 		pOutput->TaxCoefficient = 0;
-	pOutput->Netto = (pOutput->Burrito / (1 + pOutput->TaxCoefficient / 100.0)); // bypass integer division with float
+	pOutput->Netto = (int)(pOutput->Brutto / (1 + pOutput->TaxCoefficient / 100.0)); // bypass integer division with float, cast to int
 	pIndex = NULL;
 
 	return pOutput;
@@ -90,4 +88,11 @@ int MiddleName(char* pInput)
 		return 1;
 	else
 		return -1;
+}
+
+void StructUpdate(char** pIndex, char** pOutdex, int* len, char* apb)
+{
+	*pIndex = strpbrk(*pIndex + 1, apb); // update start of name
+	*pOutdex = strchr(*pIndex, ' '); // update end of name
+	*len = *pOutdex - *pIndex; // update length of word
 }
